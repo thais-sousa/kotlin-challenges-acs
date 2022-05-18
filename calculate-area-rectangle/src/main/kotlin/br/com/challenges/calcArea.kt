7644ec7f432a87dc7f0b4e8432cf2c5232f40b7c
Thais Sousa <sousatcm@gmail.com>
2022-05-18 10:29:42 -0300

    adjust - calcArea

--- calculate-area-rectangle/src/main/kotlin/br/com/challenges/calcArea.kt
+++ calculate-area-rectangle/src/main/kotlin/br/com/challenges/calcArea.kt
@@ -2,6 +2,9 @@ package br.com.challenges
 
 fun calcArea(shape: Map<String, Any>): Int {
 
-    return shape["Width"].toString().toInt() * shape["Height"].toString().toInt()
+    val result = shape["Width"].toString().toInt() * shape["Height"].toString().toInt()
+
+    return result
 
 }
+
