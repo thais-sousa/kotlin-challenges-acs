3a0fabe0385880b3cbf433f053ce6f3284a73734
Thais Sousa <sousatcm@gmail.com>
2022-05-18 10:36:21 -0300

    changing rectangle attribute - width

--- calculate-area-rectangle/src/main/kotlin/br/com/challenges/Main.kt
+++ calculate-area-rectangle/src/main/kotlin/br/com/challenges/Main.kt
@@ -6,6 +6,9 @@ fun main() {
     println("Área do Retângulo: ${calcArea(rectangle)}")
     println("Atributos do retângulo: ${toString(rectangle)}")
 
+    rectangle["Width"] = 5
+    println("Atributos do retângulo - Alteração Width: ${toString(rectangle)}")
+
 }
 
 
