4cc23e969cbe860b408bb5750b66ae38fe0bc5dd
Thais Sousa <sousatcm@gmail.com>
2022-05-18 13:20:31 -0300

    challenge FizzBuzz

---
+++ fizz-buzz/src/main/kotlin/br/com/challenges/Main.kt
@@ -0,0 +1,12 @@
+package br.com.challenges
+
+fun main() {
+
+    val numbers = 1..50
+    fizzBuzz(numbers)
+
+}
+
+
+
+
---
+++ fizz-buzz/src/main/kotlin/br/com/challenges/fizzBuzz.kt
@@ -0,0 +1,24 @@
+package br.com.challenges
+
+fun fizzBuzz(numbers: IntRange) {
+
+    for (n in numbers) {
+        when {
+            n % 3 == 0 && n % 5 == 0 -> println("FizzBuzz")
+            n % 3 == 0 && n % 5 != 0 -> println("Fizz")
+            n % 3 != 0 && n % 5 == 0 -> println("Buzz")
+            else -> println(n)
+        }
+
+//        if (n % 3 == 0 && n % 5 == 0) {
+//            println("FizzBuzz")
+//        } else if (n % 3 == 0) {
+//            println("Fizz")
+//        } else if (n % 5 == 0) {
+//            println("Buzz")
+//        } else {
+//            println(n)
+//        }
+//    }
+    }
+}
