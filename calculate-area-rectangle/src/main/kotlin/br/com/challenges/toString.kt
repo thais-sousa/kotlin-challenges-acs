912d225ad631f982e03a6d52ee7383fda7733033
Thais Sousa <sousatcm@gmail.com>
2022-05-18 10:47:40 -0300

    changing toString function

--- calculate-area-rectangle/src/main/kotlin/br/com/challenges/toString.kt
+++ calculate-area-rectangle/src/main/kotlin/br/com/challenges/toString.kt
@@ -2,10 +2,8 @@ package br.com.challenges
 
 fun toString(shape : Map<String, Any>) : String {
 
-    val rectangle = mutableMapOf("Width" to 10, "Height" to 10, "Color" to "Red")
-
     return "Width: ${shape["Width"]}, " +
             "Height: ${shape["Height"]}, " +
             "Color: ${shape["Color"]}, " +
-            "Área do retângulo: ${calcArea(rectangle)}"
+            "Área do retângulo: ${calcArea(shape)}"
 }
