8295bce477d2837e083bfb1dc9ee23fdf74504bb
Thais Sousa <sousatcm@gmail.com>
2022-05-20 10:55:36 -0300

    organizing the code

---
+++ fibonacci/src/main/kotlin/br/com/challenges/fibonacci.kt
@@ -0,0 +1,19 @@
+package br.com.challenges
+
+fun fibonacci(numbers: IntRange) {
+
+    var firstTerm = 0
+    var secondTerm = 1
+
+    print("Primeiros ${numbers.count()} termos da Sequência de Fibonacci: ")
+
+    for (number in numbers) {
+        print("$firstTerm, ")
+
+        val sumOfTerms = firstTerm + secondTerm
+        firstTerm = secondTerm
+        secondTerm = sumOfTerms
+
+    }
+
+}
