d182aa8d2a2a34f1a6128ac4b2e30c23f7e996ec
Thais Sousa <sousatcm@gmail.com>
2022-05-25 20:54:22 -0300

    implementing deposit method

--- bank-account/src/main/kotlin/br/com/challenges/Main.kt
+++ bank-account/src/main/kotlin/br/com/challenges/Main.kt
@@ -1,10 +1,21 @@
 package br.com.challenges
 fun main() {
 
+    val contaThais = Acount(numero = 1001, titular = "Thais", saldo = 100.0)
+    contaThais.deposit(1000.0)
+    println(contaThais.saldo)
+
 }
 
 class Acount(
     val numero: Int,
     val titular: String,
     var saldo: Double
-)
+) {
+    fun deposit(value: Double) {
+        when {
+            value < 10000.0 -> saldo += value
+            else -> println("Não é possível depositar este valor. O valor máximo permitido para este depósito é de R$ 10.000,00")
+        }
+    }
+}
