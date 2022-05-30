e666e52c28a488f4dc8007d4d9f25f37e63c4428
Thais Sousa <sousatcm@gmail.com>
2022-05-30 11:05:27 -0300

    adjusting account balance property and performing tests

--- bank-account/src/main/kotlin/br/com/challenges/Main.kt
+++ bank-account/src/main/kotlin/br/com/challenges/Main.kt
@@ -2,46 +2,71 @@ package br.com.challenges
 
 fun main() {
 
-    val contaThais = Account(numeroCta = 1001, titular = "Thais")
-    contaThais.deposit(150.0)
-    println(contaThais.accountBalance)
-    contaThais.sac(-50.0)
-    println("conta após saque: ${contaThais.accountBalance}")
+    //Criando a conta e testando os métodos
+    val accountThais = Account(numberAccount = 1001, nameClient = "Thais", accountBalance = 100.0)
+    println("Dados da Conta ${accountThais.numberAccount}: Nome do Titular = ${accountThais.nameClient}, Saldo da conta ${accountThais.accountBalance}")
+    accountThais.deposit(200.0)
+    println("Saldo conta Thais após depósito: ${accountThais.accountBalance}")
 
-    val contaMarco = Account(numeroCta = 1002, titular = "Marco")
-    contaThais.transfer(contaMarco, 10000.0)
-    println(contaMarco.accountBalance)
-    println(contaThais.accountBalance)
+    accountThais.sac(50.0)
+    println("Saldo conta Thais após saque: ${accountThais.accountBalance}")
 
+    println()
+
+    //Criando uma nova conta e testando os métodos
+    val accountMonteiro = Account(numberAccount = 1002, nameClient = "Monteiro", accountBalance = 0.0)
+    println("Dados da Conta ${accountMonteiro.numberAccount}: Nome do Titular = ${accountMonteiro.nameClient}, Saldo da conta ${accountMonteiro.accountBalance}")
+    accountMonteiro.deposit(100.0)
+    println("Saldo conta Monteiro após depósito: ${accountMonteiro.accountBalance}")
+
+    accountMonteiro.sac(30.0)
+    println("Saldo conta Monteiro após saque: ${accountMonteiro.accountBalance}")
+
+    println()
+
+    //Testando transferências
+    accountThais.transfer(accountMonteiro, 50.0)
+    println("Saldo conta Monteiro após receber transferência: ${accountMonteiro.accountBalance}")
+    println("Saldo conta Thais após realizar transferência: ${accountThais.accountBalance}")
+
+    accountMonteiro.transfer(accountThais, 30.0)
+    println("Saldo conta Thais após receber transferência: ${accountThais.accountBalance}")
+    println("Saldo conta Monteiro após realizar transferência: ${accountMonteiro.accountBalance}")
+
+    //Testando transferência acima do valor permitido
+    accountThais.transfer(accountMonteiro, 50000.0)
 }
 
 class Account(
-    numeroCta: Int,
-    titular: String
+    val numberAccount: Int,
+    val nameClient: String,
+    accountBalance: Double,
 ) {
-    var accountBalance = 0.0
+    var accountBalance = accountBalance
         private set
 
     val limiteValue = 10000.0
     fun deposit(value: Double) {
         when {
-            value > 0 && value <= limiteValue -> this.accountBalance += value
-            else -> throw IllegalArgumentException("O valor para depósito deve ser positivo e de no máximo R$ 10.000,00")
+            value > 0 && value <= limiteValue -> accountBalance += value
+            else -> throw IllegalArgumentException("O valor para depósito ou transferência deve ser positivo e de no máximo R$ 10.000,00")
         }
     }
 
     fun sac(value: Double) {
-        when {
-            accountBalance > 0 && accountBalance >= value -> accountBalance -= value
+        when (value > 0) {
+            (accountBalance > 0 && accountBalance >= value) -> accountBalance -= value
             else -> throw IllegalArgumentException("Não é possível sacar este valor!")
         }
     }
 
     fun transfer(destinationAccount: Account, value: Double) {
-        if (accountBalance >= value && value <= limiteValue) {
-            accountBalance -= value
-            destinationAccount.deposit(value)
-        } else
-            throw IllegalArgumentException("Não é permitido transferir este valor!")
+        when {
+            accountBalance >= value && value <= limiteValue -> {
+                accountBalance -= value
+                destinationAccount.deposit(value)
+            }
+            else -> throw IllegalArgumentException("Não é permitido transferir este valor!")
+        }
     }
 }
