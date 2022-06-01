4818f8af7bc80d9fc2c94aac236e8fcb4486d994
Thais Sousa <sousatcm@gmail.com>
2022-05-31 22:11:34 -0300

    changing oviparous class to interface

--- inheritance/src/main/kotlin/br/com/challenges/Oviparous.kt
+++ inheritance/src/main/kotlin/br/com/challenges/Oviparous.kt
@@ -1,7 +1,6 @@
 package br.com.challenges
 
-open class Oviparous {
-    fun oviposition(): String {
-        return "Bota ovos"
-    }
+interface Oviparous : Animal {
+    fun oviposition(): String
+
 }
